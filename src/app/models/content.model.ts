fc7f7524c6d6c38ec1da94ad26f2a90971e1a8ec
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-09-23 18:36:09 -0300

    Actualiza modelo

--- src/app/models/content.model.ts
+++ src/app/models/content.model.ts
@@ -1,18 +1,8 @@
 import { TextBlockContent } from '@models/block-content.model';
 
-export interface ContentCampaign {
-	title: string;
-	shortTitle: string;
-	shortDescription: string;
-	description: string;
-	smallImageUrl: string;
-	largeImageUrl: string;
-	url: string;
-}
-
 type Viewport = 'xs' | 'md';
 
-export interface ContentCampaignObj {
+export interface ContentCampaign {
 	slug: string;
 	url: string;
 	description: TextBlockContent[];
