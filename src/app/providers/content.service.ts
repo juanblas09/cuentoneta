98593f72c06f69279a0748b9e18f4d68d1af0e43
Ramiro Olivencia <32349705+rolivencia@users.noreply.github.com>
2024-09-23 18:41:37 -0300

    Adapta frontend a modelo

--- src/app/providers/content.service.ts
+++ src/app/providers/content.service.ts
@@ -1,6 +1,6 @@
 // Core
 import { inject, Injectable } from '@angular/core';
-import { Observable, of } from 'rxjs';
+import { map, Observable, of } from 'rxjs';
 
 // Interfaces
 import { ContentCampaign } from '@models/content.model';
@@ -24,34 +24,6 @@ export class ContentService {
 	}
 
 	public getContentCampaigns$(): Observable<ContentCampaign[]> {
-		return of([
-			{
-				shortTitle: 'Desde el barrio de Flores...',
-				title: 'Desde el barrio de Flores...',
-				shortDescription: '... la entrega #1 de este nuevo ciclo',
-				description: '... la entrega #1 del ciclo "Pluma de la semana"',
-				smallImageUrl: './assets/img/pluma-de-la-semana-1-small.jpg',
-				largeImageUrl: './assets/img/pluma-de-la-semana-1.jpg',
-				url: '../author/alejandro-dolina',
-			},
-			{
-				shortTitle: 'Los cuentos de iSAT',
-				title: 'Los cuentos de iSAT',
-				shortDescription: '...recopilados en texto y video',
-				description: '...recopilados en texto y video con narración de Alberto Laiseca',
-				smallImageUrl: './assets/img/cuentos-de-terror-small.jpg',
-				largeImageUrl: './assets/img/cuentos-de-terror.jpg',
-				url: '../storylist/cuentos-de-terror-de-alberto-laiseca',
-			},
-			{
-				shortTitle: '📗 Cuentos clásicos...',
-				title: '📗 De ediciones Proyecto Territorio...',
-				shortDescription: '... por autores santafesinos',
-				description: '... una selección de cuentos clásicos de plumas santafesinas',
-				smallImageUrl: './assets/img/ciudades-campos-pueblos-islas-small.jpg',
-				largeImageUrl: './assets/img/ciudades-campos-pueblos-islas.jpg',
-				url: '../storylist/cuentos-de-terror-de-alberto-laiseca',
-			},
-		]);
+		return this.getLandingPageContent().pipe(map((content) => content.campaigns));
 	}
 }
