0c521159c8de1a6023084909ff6007131accc94d
Ramiro Olivencia <ramiro@olivencia.com.ar>
2024-12-22 23:58:52 -0300

    test1

--- src/ssr.server.ts
+++ src/ssr.server.ts
@@ -9,35 +9,32 @@ import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
 import routes from './api/routes';
 
-const serverDistFolder = dirname(fileURLToPath(import.meta.url));
-const browserDistFolder = resolve(serverDistFolder, '../browser');
-
 export function app(): express.Express {
 	const server = express();
-	const angularApp = new AngularNodeAppEngine();
+	const serverDistFolder = dirname(fileURLToPath(import.meta.url));
+	const browserDistFolder = resolve(serverDistFolder, '../browser');
+
+	// Here, we now use the `AngularNodeAppEngine` instead of the `CommonEngine`
+	const angularNodeAppEngine = new AngularNodeAppEngine();
 
 	// Registra las routes utilizadas por la API
 	for (const route of routes) {
 		server.use(`/api${route.path}`, route.controller);
 	}
 
-	/**
-	 * Serve static files from /browser
-	 */
-	server.use(
+	server.get(
+		'**',
 		express.static(browserDistFolder, {
 			maxAge: '1y',
-			index: false,
-			redirect: false,
+			index: 'index.html',
 		}),
 	);
 
-	/**
-	 * Handle all other requests by rendering the Angular application.
-	 */
-	server.use('/**', (req, res, next) => {
-		angularApp
-			.handle(req)
+	server.get('**', (req, res, next) => {
+		console.log('request', req.url, res.status);
+
+		angularNodeAppEngine
+			.handle(req, { server: 'express' })
 			.then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
 			.catch(next);
 	});
