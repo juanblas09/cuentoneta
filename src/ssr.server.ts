1bbe748efb37a2b6efa302188ca64721a9decdd6
Ramiro Olivencia <ramiro@olivencia.com.ar>
2024-12-23 01:25:22 -0300

    another test

--- src/ssr.server.ts
+++ src/ssr.server.ts
@@ -1,18 +1,20 @@
-import { APP_BASE_HREF } from '@angular/common';
-import { CommonEngine, isMainModule } from '@angular/ssr/node';
+import {
+	AngularNodeAppEngine,
+	createNodeRequestHandler,
+	isMainModule,
+	writeResponseToNodeResponse,
+} from '@angular/ssr/node';
 import express from 'express';
-import { dirname, join, resolve } from 'node:path';
+import { dirname, resolve } from 'node:path';
 import { fileURLToPath } from 'node:url';
-import bootstrap from './main.server';
 import routes from './api/routes';
 
 const serverDistFolder = dirname(fileURLToPath(import.meta.url));
 const browserDistFolder = resolve(serverDistFolder, '../browser');
-const indexHtml = join(serverDistFolder, 'index.server.html');
 
 export function app(): express.Express {
 	const server = express();
-	const commonEngine = new CommonEngine();
+	const angularApp = new AngularNodeAppEngine();
 
 	// Registra las routes utilizadas por la API
 	for (const route of routes) {
@@ -22,43 +24,43 @@ export function app(): express.Express {
 	/**
 	 * Serve static files from /browser
 	 */
-	server.get(
-		'**',
+	server.use(
 		express.static(browserDistFolder, {
 			maxAge: '1y',
-			index: 'index.html',
+			index: false,
+			redirect: false,
 		}),
 	);
 
 	/**
 	 * Handle all other requests by rendering the Angular application.
 	 */
-	server.get('**', (req, res, next) => {
-		const { protocol, originalUrl, baseUrl, headers } = req;
-
-		commonEngine
-			.render({
-				bootstrap,
-				documentFilePath: indexHtml,
-				url: `${protocol}://${headers.host}${originalUrl}`,
-				publicPath: browserDistFolder,
-				providers: [{ provide: APP_BASE_HREF, useValue: baseUrl }],
-			})
-			.then((html) => res.send(html))
-			.catch((err) => next(err));
+	server.use('/**', (req, res, next) => {
+		angularApp
+			.handle(req)
+			.then((response) => (response ? writeResponseToNodeResponse(response, res) : next()))
+			.catch(next);
 	});
 
 	return server;
 }
 
-const server = app();
 /**
  * Start the server if this module is the main entry point.
  * The server listens on the port defined by the `PORT` environment variable, or defaults to 4000.
  */
+const server = app();
 if (isMainModule(import.meta.url)) {
 	const port = process.env['PORT'] || 4000;
 	server.listen(port, () => {
-		console.log(`Node Express server listening on http://localhost:${port}`);
+		console.log(`Aplicación en modo Server-Side Rendering corriendo en http://localhost:${port}`);
+		if (port === 4000) {
+			console.log(`Aplicación en modo Client-Side Rendering corriendo en  http://localhost:4200`);
+		}
 	});
 }
+
+/**
+ * The request handler used by the Angular CLI (dev-server and during build).
+ */
+export const reqHandler = createNodeRequestHandler(server);
